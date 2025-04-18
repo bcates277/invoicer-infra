98c9f3b4d2d60c998fac894bc423319d56b55f7c
Betsy Cates <betsycates@betsycates-macbook-air.tailcb7ae.ts.net>
2025-04-18 11:32:15 -0400

    update

--- infra/index.ts
+++ infra/index.ts
@@ -1,13 +1,12 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import { endpointUrl, envId } from "./ebs";
-import { bucketName } from "./s3";
+import { bucketName } from "./s3/s3";
 import { securityGroupId, dbInstanceEndpoint } from "./rds";
 
 import "./ebs";
-import "./s3";
+import "./s3/s3";
 import "./rds";
-
 
 // export
 export { endpointUrl, envId, bucketName, securityGroupId, dbInstanceEndpoint };
